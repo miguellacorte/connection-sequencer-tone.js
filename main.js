d168830f9a36f95127cf3a82302b09a8ab7d40f9
miguellacorte <76566384+miguellacorte@users.noreply.github.com>
2022-06-16 15:37:02 +0200

    sound done :)

--- main.js
+++ main.js
@@ -1,145 +1,277 @@
+// const { Tone } = require("tone/build/esm/core/Tone");
 
-let img;
-function preload() {
-  img = loadImage('./tumblr_626b942b4687a563df69ce355610e66b_aaed3d79_1280.jpg');
+// const { Sequence } = require("tone");
+
+// let notes = [];
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
 }
 
-
 document.querySelector('button')?.addEventListener('click', async () => {
-  Tone.start()
+  // Tone.start()
   console.log('audio is ready')
 })
 
- const synth = new Tone.Synth({ oscillator: { type: "square8" } }).toDestination();
+
+
+
+
+let synth = new Tone.Synth({ oscillator: { type: "square8" } }).toDestination();
+
+ Tone.Master.volume.value = -15;
+
+//  if there are more than 10 clicks > make reverb higher (select parameter)
+// start with high ping pong and slowly reduce
+
+ let reverb = new Tone.Reverb([1]).toDestination();
+ reverb.wet.rampTo(1, '8n');
+
+ let chorus = new Tone.Chorus(20, 220, 1).toDestination();
+//  chorus.wet.value = 1
+
+let feedback = 0.8;
+ let pingPong = new Tone.PingPongDelay("16n", feedback).toDestination();
+
  
 
+synth.connect(chorus)
+chorus.connect(pingPong)
+pingPong.connect(reverb)
+// delay.connect(reverb)
 
 
 
-function setup() {
- 
-  createCanvas(windowWidth, windowHeight);
 
+function mousePressed() {
+
+  let notes = [];
+
+
+  // ROW 1
+
+  // X1 Y1
+if ( (mouseX <= width/4 ) && (mouseY <= height/4 ) && (mouseX > 1) && (mouseY > 1)) {
+    notes.push("G4");
+      console.log(notes) 
+      
+  }
+
+  // X2 Y1
+if ( (mouseX > width/4) && (mouseX < width/4* 2) && (mouseY < height/4 ) ) {
+    // synth.triggerAttackRelease("D4", "8n");
+    notes.push("A4"); 
+    console.log(notes)
+}
+
+  // X3 Y1
+if ( (mouseX > width/4* 2) && (mouseX < width/4* 3) && (mouseY < height/4 ) ) {
+  // synth.triggerAttackRelease("D4", "8n");
+  notes.push("Bb4");
+  console.log(notes)
+}
+
+  // X4 Y1
+if ( (mouseX > width/4* 3) && (mouseX < width/4* 4) && (mouseY < height/4 ) ) {
+  // synth.triggerAttackRelease("D4", "8n");
+  notes.push("C4");
+  console.log(notes)
+}
+
+// ROW 2
+
+  // X1 Y2
+if ( (mouseX <= width/4 ) && (mouseY <= height/2 ) && (mouseY > height/4 ) && (mouseX > 1) && (mouseY > 1) ){
+    notes.push("D3");
+      console.log(notes) 
+      
+  }
+
+  // X2 Y2
+  if ( (mouseX > width/4  ) && (mouseX < width/4* 2) && (mouseY <= height/2 ) && (mouseY > height/4 ) && (mouseX > 1) && (mouseY > 1) ){
+    notes.push("Bb3");
+      console.log(notes) 
+      
+  }
+
+  // X3 Y2
+  if ( (mouseX > width/4* 2) && (mouseX < width/4* 3) && (mouseY <= height/2 ) && (mouseY > height/4 ) && (mouseX > 1) && (mouseY > 1) ){
+    notes.push("F3");
+      console.log(notes) 
+      
+  }
+
+   // X4 Y2
+   if ( (mouseX > width/4* 3) && (mouseX < width/4* 4) && (mouseY <= height/2 ) && (mouseY > height/4 ) && (mouseX > 1) && (mouseY > 1) ){
+    notes.push("G3");
+      console.log(notes) 
+      
+  }
+
+// ROW 3
+
+  // X1 Y3
+if ( (mouseX <= width/4 ) && (mouseY > height/2 ) && (mouseY < height/2 + height/4) && (mouseX > 1) && (mouseY > 1)) {
+  notes.push("A2");
+    console.log(notes) 
+    
+}
+
+// X2 Y3
+if ( (mouseX > width/4) && (mouseX < width/4* 2) && (mouseY > height/2 ) && (mouseY < height/2 + height/4)  ) {
+  // synth.triggerAttackRelease("D4", "8n");
+  notes.push("F4");
   
-        
- 
+  console.log(notes)
+}
+
+// X3 Y3
+if ( (mouseX > width/4* 2) && (mouseX < width/4* 3) && (mouseY > height/2 ) && (mouseY < height/2 + height/4)   ) {
+// synth.triggerAttackRelease("D4", "8n");
+notes.push("C4");
+console.log(notes)
+}
+
+// X4 Y3
+if ( (mouseX > width/4* 3) && (mouseX < width/4* 4) && (mouseY > height/2 ) && (mouseY < height/2 + height/4) ) {
+// synth.triggerAttackRelease("D4", "8n");
+notes.push("D4");
+console.log(notes)
+}
+
+// ROW 4
+
+// X1 Y4
+if ( (mouseX <= width/4 ) && (mouseY > height/2 + height/4)  && (mouseX > 1) && (mouseY > 1) ){
+  notes.push("G3");
+    console.log(notes) 
+    
+}
+
+// X2 Y4
+if ( (mouseX > width/4  ) && (mouseX < width/4* 2) && (mouseY > height/2 + height/4)  && (mouseX > 1) && (mouseY > 1) ){
+  notes.push("A3");
+    console.log(notes) 
+    
+}
+
+// X3 Y4
+if ( (mouseX > width/4* 2) && (mouseX < width/4* 3) && (mouseY > height/2 + height/4) && (mouseX > 1) && (mouseY > 1) ){
+  notes.push("Bb3");
+    console.log(notes) 
+    
+}
+
+ // X4 Y4
+ if ( (mouseX > width/4* 3) && (mouseX < width/4* 4) && (mouseY > height/2 + height/4)  && (mouseX > 1) && (mouseY > 1) ){
+  notes.push("C3");
+    console.log(notes) 
+    
+}
+
+
+Tone.Transport.bpm.value = 30;
+
+Tone.Transport.pause();
+const seq = new Tone.Sequence((time, note) => {
+  synth.triggerAttackRelease(note, 0.1, time);
+  console.log(seq.state)
+  
+},notes, "4n").start();
+
+
+
+
+
+Tone.Transport.start();
+
+
+
+
+
+
+
 
 }
-    
-  
-// function mousePressed() {
-//   loop();
-  
-//   //line
-//   strokeWeight(4);
-//   fill(230, 11, 07);
-//   line(pmouseX - 3, pmouseY + 3, mouseX, mouseY);
 
-//   //circle
-//   strokeWeight(0);
-//   fill(0);
-  
-//   circle(mouseX, mouseY, 29);
-
-//   
-// }
 
 
 function draw() {
-  background(255);
-      
+
+
+
   
-    
-      console.log(mouseY)
+  
+  
+  
+  
       
 // X1 Y0
-      if ( (mouseX <= width/6 ) && (mouseY <= height/6 ) && (mouseX > 1) && (mouseY > 1) && (mouseIsPressed === true) ) {
+    //   if ( (mouseX <= width/4 ) && (mouseY <= height/4 ) && (mouseX > 1) && (mouseY > 1) && (mouseIsPressed === true) ) {
+    //     fill(35,0,0);
+    //     rect(width/4,0,width/4,height/4); 
 
-        // image(img, 0, 0);
-
-        function mousePressed() {
-          noLoop();
-        }
-        
-      fill(255,0,0);
-      rect(0,0,width/6,height/6); 
-      synth.triggerAttackRelease("C4", "8n");
+    // }
       
-     function mouseReleased() {
-        loop();
-      }
-
       
-
-      
-    }
-      
-      // console.log(mouseX)
-      // console.log(width/6*2)
-      
-//   X2 YO
-      if ( ((mouseX > width/6) && (mouseX < width/6* 2)) && (mouseY < height/6 ) ) {
-        console.log(height/2)
-        fill(35,0,0);
-        rect(width/6,0,width/6,height/6); 
+// //   X2 YO
+//       if ( ((mouseX > width/4) && (mouseX < width/4* 2)) && (mouseY < height/4 ) ) {
+//         console.log(height/2)
+//         
           
-        }
+//         }
       
-//   X3 Y0
-      if ( ((mouseX > width/6 * 2) && (mouseX < width/6* 3)) && (mouseY < height/6 ) ) {
-        console.log(height/2)
-        fill(35,0,0);
-        rect(width/6 * 2,0,width/6,height/6); 
+// //   X3 Y0
+//       if ( ((mouseX > width/4 * 2) && (mouseX < width/4* 3)) && (mouseY < height/4 ) ) {
+//         console.log(height/2)
+//         fill(35,0,0);
+//         rect(width/4 * 2,0,width/4,height/4); 
           
-        }
+//         }
       
-//   X4 Y0
-      if ( ((mouseX > width/6 * 3) && (mouseX < width/6* 4)) && (mouseY < height/6 ) ) {
-        console.log(height/2)
-        fill(35,0,0);
-        rect(width/6 * 3,0,width/6,height/6); 
+// //   X4 Y0
+//       if ( ((mouseX > width/4 * 3) && (mouseX < width/4* 4)) && (mouseY < height/4 ) ) {
+//         console.log(height/2)
+//         fill(35,0,0);
+//         rect(width/4 * 3,0,width/4,height/4); 
         
-        }
+//         }
       
-//   X5 Y0
-       if ( ((mouseX > width/6 * 4) && (mouseX < width/6* 5)) && (mouseY < height/6 ) ) {
-        console.log(height/2)
-        fill(35,0,0);
-        rect(width/6 * 4,0,width/6,height/6); 
+//   X1 Y1
+    //   if ( (mouseX <= width/4 ) && (mouseY <= height/2 ) && (mouseY > height/4 ) && (mouseX > 1) && (mouseY > 1) && (mouseIsPressed === true) ) {
+    //     fill(100,89,89);
+    //     rect(width/4,0,width/4,height/4); 
+
+    // }
         
-        }
+//         }
       
-//   X6 Y0
-       if ( ((mouseX > width/6 * 5) && (mouseX < width/6* 6)) && (mouseY < height/6 ) ) {
-        console.log(height/2)
-        fill(35,0,0);
-        rect(width/6 * 5,0,width/6,height/6); 
+// //   X4 Y0
+//        if ( ((mouseX > width/4 * 5) && (mouseX < width/4* 4)) && (mouseY < height/4 ) ) {
+//         console.log(height/2)
+//         fill(35,0,0);
+//         rect(width/4 * 5,0,width/4,height/4); 
         
-        }
+//         }
       
-//   X7 Y0
-       if ( ((mouseX > width/6 * 5) && (mouseX < width/6* 7)) && (mouseY < height/6 ) ) {
-        console.log(height/2)
-        fill(35,0,0);
-        rect(width/6 * 5,0,width/6,height/6); 
+// //   X7 Y0
+//        if ( ((mouseX > width/4 * 5) && (mouseX < width/4* 7)) && (mouseY < height/4 ) ) {
+//         console.log(height/2)
+//         fill(35,0,0);
+//         rect(width/4 * 5,0,width/4,height/4); 
         
-        }
+//         }
       
-      
-      
-      
-      
-      
-        // else if ( (mouseX > width/3) && (mouseX < 2*width/3) ) {
-        // fill(255,0,0);
-        // rect(width/3,0,width/3,height);
-        // }		else if ( (mouseX>2*width/3) && (mouseX < width)) {
-        // fill(255,0,0);
-        // rect(2*width/3,0,width/3,height);
-        // }
     
-    }
+}
+
+
+
+
+
+
+
     
-    function windowResized() {
+function windowResized() {
       resizeCanvas(windowWidth, windowHeight);
-    }
+    };
